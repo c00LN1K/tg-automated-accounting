d4f737cac72b022303965f08ca94d37dfddd48e3
Nikita <138148415+c00LN1K@users.noreply.github.com>
2024-02-17 20:48:30 +0300

    1.0
    Upgrade telegram bot - add DB and new way of work with excel

---
+++ db.py
@@ -0,0 +1,105 @@
+from datetime import datetime
+
+from sqlalchemy import create_engine, Column, Integer, DATETIME, VARCHAR, TIME, Boolean, and_, DATE
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+engine = create_engine('sqlite:///mydb.db')
+session = sessionmaker(bind=engine)()
+
+Base = declarative_base()
+
+
+class BaseModel(Base):
+    __abstract__ = True
+
+    id = Column(Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
+
+    def __repr__(self):
+        return f"<{__class__.__name__}(id={self.id})>"
+
+
+class Task(BaseModel):
+    __tablename__ = 'tasks'
+
+    day = Column(Integer)
+    month = Column(Integer)
+    year = Column(Integer)
+    name = Column(VARCHAR(255))
+    time = Column(TIME)
+    duration = Column(Integer)
+    price = Column(Integer)
+    is_finished = Column(Boolean)
+
+    def __init__(self, date: datetime.date, name, time, duration, price, is_finished=False):
+        self.day, self.month, self.year = date.day, date.month, date.year
+        self.name = name
+        self.time = time
+        self.duration = duration
+        self.price = price
+        self.is_finished = is_finished
+
+    @staticmethod
+    def add_task(task: list):
+        try:
+            new_task = Task(*task)
+            if session.query(Task).filter(
+                    and_(Task.year == task[0].year,
+                         Task.month == task[0].month,
+                         Task.day == task[0].day,
+                         Task.time == task[2])).first():
+                return False
+            session.add(new_task)
+            session.commit()
+            return True
+        except Exception as ex:
+            print('Error in func add_task -', ex)
+            session.rollback()
+            return False
+
+    @staticmethod
+    def get_unfinished_tasks():
+        return session.query(Task).filter(Task.is_finished == False).order_by(Task.month, Task.day, Task.time).all()
+
+    @staticmethod
+    def delete_task(pk):
+        try:
+            task = session.get(Task, pk)
+            if task:
+                session.delete(task)
+                session.commit()
+        except Exception as ex:
+            print('Error in func delete_task -', ex)
+            session.rollback()
+
+    @staticmethod
+    def finish_task(pk):
+        try:
+            task = session.get(Task, pk)
+            if task:
+                task.is_finished = True
+                session.commit()
+        except Exception as ex:
+            print('Error in func finish_task -', ex)
+            session.rollback()
+
+    @staticmethod
+    def get_by_day(date: datetime.date):
+        return session.query(Task).filter(
+            and_(Task.year == date.year, Task.month == date.month, Task.day == date.day)).order_by(Task.time).all()
+
+    @staticmethod
+    def get_by_month(date: datetime.date):
+        return session.query(Task).filter(
+            and_(Task.year == date.year, Task.month == date.month)).order_by(Task.day, Task.time).all()
+
+    @staticmethod
+    def get_by_year(date: datetime.date):
+        return session.query(Task).filter(Task.year == date.year).order_by(Task.month, Task.day, Task.time).all()
+
+    @staticmethod
+    def get_all_tasks():
+        return session.query(Task).order_by(Task.year, Task.month, Task.day, Task.time).all()
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
--- work_with_excel.py
+++ work_with_excel.py
@@ -8,6 +8,8 @@ import numpy as np
 import pandas as pd
 from openpyxl.styles import Font
 
+from db import Task
+
 MONTH = ['', 'январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь',
          'декабрь']
 WEEKDAY = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье']
@@ -23,159 +25,63 @@ def check_root():
         os.chdir(ROOT)
 
 
-def save_active_tasks():
-    check_root()
-    with open('task.json', 'w') as file:
-        json.dump(active_tasks, file)
-
-
-def get_active_tasks():
-    check_root()
-    if 'task.json' in os.listdir():
-        with open('task.json', 'r') as file:
-            new_dict = json.load(file)
-            active_tasks.update(new_dict)
-
-
-def get_filename(date, is_create=True):
-    check_root()
-    dir_year = f'{date.year}'
-    # Дисбаланс из-за того, что мы берем месяц от переданного дня, а начало-конец недели может быть в разных месяцах
-    # Из-за этого получается дублирование. И одном и другом месяце будут содержаться те же недели
-    # Решение, брать месяц из первого дня недели
+def get_filename(date):
     first_day_week = date - datetime.timedelta(days=date.weekday())
-    dir_month = f'{MONTH[first_day_week.month]}'
-
     last_day_week = (date + datetime.timedelta(days=6 - date.weekday())).strftime("%d.%m")
     excel_week = f'{first_day_week.strftime("%d.%m")}-{last_day_week}' + '.xlsx'
-    sheet = f'{date.day:02}'
 
-    if 'excel' not in os.listdir():
-        raise Exception('No such directory excel!')
-    os.chdir('excel')
-
-    if dir_year not in os.listdir():
-        if not is_create:
-            return
-        os.mkdir(dir_year)
-    os.chdir(dir_year)
-
-    if dir_month not in os.listdir():
-        if not is_create:
-            return
-        os.mkdir(dir_month)
-    os.chdir(dir_month)
-
-    if excel_week not in os.listdir():
-        if not is_create:
-            return
-        workbook = openpyxl.Workbook()
-        ws = workbook.active
-        ws.title = sheet
-        workbook.save(excel_week)
-        df = pd.DataFrame(default_data)
-        with pd.ExcelWriter(excel_week, mode='a', engine='openpyxl',
-                            if_sheet_exists='replace') as writer:
-            df.to_excel(writer, sheet_name=sheet, index=False)
-
-    wb = openpyxl.load_workbook(excel_week)
-    if sheet not in wb.sheetnames:
-        if not is_create:
-            return
-        wb.save(excel_week)
-        df = pd.DataFrame(default_data)
-        with pd.ExcelWriter(excel_week, mode='a', engine='openpyxl',
-                            if_sheet_exists='replace') as writer:
-            df.to_excel(writer, sheet_name=sheet, index=False)
-    wb.close()
-
-    return os.path.join(os.getcwd(), excel_week)
+    return excel_week
 
 
-def write_task(task: list):
-    """
-    Функция, которая добавляет новую запись
-    """
-    date = task[0]
-    print(task)
-    file_name = get_filename(datetime.datetime.fromisoformat(date))
-    sheet = date[-2:]
+def create_excel_file(data, filename):
+    df = pd.DataFrame(default_data)
+    for el in data:
+        df.loc[len(df.index)] = [f'{el.day}.{el.month}.{el.year}', el.name, el.time.strftime('%H:%M'), el.duration,
+                                 el.price, el.is_finished]
+    df.to_excel(filename, index=False)
 
-    # Чтение данных из листа
-    df = pd.read_excel(file_name, sheet_name=sheet, usecols=f"A:{chr(len(default_data) - 1 + ord('A'))}")
     summ = np.sum(df[df['Оплачено'] == True]['Стоимость'])
-    # Проверка наличия задачи в DataFrame
-    mask = (df == task).all(axis=1)
-    if mask.any():
-        print("Такая задача уже существует.")
-        return False
-    else:
-        # Добавление новой строки в DataFrame
-        df.loc[len(df)] = task
-        df.sort_values(by='Время', inplace=True)
 
-        # Записываем DataFrame в тот же лист
-        with pd.ExcelWriter(file_name, mode='a', engine='openpyxl',
-                            if_sheet_exists='replace') as writer:
-            df.to_excel(writer, sheet_name=sheet, index=False)
-
-    wb = openpyxl.load_workbook(filename=file_name)
-    ws = wb.get_sheet_by_name(sheet)
+    wb = openpyxl.load_workbook(filename=filename)
+    ws = wb.get_sheet_by_name(wb.sheetnames[0])
     ws['I1'] = 'Сумма'
     ws['I1'].font = Font(bold=True)
     ws['I2'] = summ
-    wb.save(file_name)
+    wb.save(filename)
     wb.close()
-    global last_index
-    while True:
-        if active_tasks.get(str(last_index)):
-            last_index += 1
-        else:
-            active_tasks[str(last_index)] = task
-            break
-
-    last_index += 1
-    print('Обновление индекса:', last_index)
-    return True
 
 
-def sort_and_save_active_tasks():
-    new_dict = dict(sorted(active_tasks.items(), key=lambda x: (x[1][0], x[1][2])))
-    active_tasks.clear()
-    active_tasks.update(new_dict)
-    save_active_tasks()
+def create_one_day_excel_file(date: datetime.date):
+    filename = f'{date.strftime("%d.%m.%Y")}.xlsx'
+    data = Task.get_by_day(date)
+    if not data:
+        return
+    create_excel_file(data, filename)
+    return filename
 
-def change_status(status, task: list):
-    """
-    Функция, которая будет изменять статус записи (удалить или завершить)
-    """
 
-    date = task[0]
-    file_name = get_filename(datetime.datetime.fromisoformat(date))
-    sheet = date[-2:]
-    summ = 0
-    with pd.ExcelWriter(file_name, engine="openpyxl", mode="a",
-                        if_sheet_exists="replace") as writer:
-        df = pd.read_excel(file_name, sheet_name=sheet, usecols=f"A:{chr(len(default_data) - 1 + ord('A'))}")
-        mask = (df == task).all(axis=1)
-        if mask.any():
-            location = df.index[mask].tolist()
-            print(df.loc[location], location, status)
-            if status == 'del':
-                df.drop(location, inplace=True)
-            else:
-                df.loc[location, 'Оплачено'] = True
+def create_month_excel_file(date: datetime.date):
+    filename = f'{MONTH[date.month]}.xlsx'
+    data = Task.get_by_month(date)
+    if not data:
+        return
+    create_excel_file(data, filename)
+    return filename
 
-            summ = np.sum(df[df['Оплачено'] == True]['Стоимость'])
-            df.to_excel(writer, sheet_name=sheet, index=False)
-        else:
-            print("Строка не найдена в DataFrame.")
 
-    if summ:
-        wb = openpyxl.load_workbook(filename=file_name)
-        ws = wb.get_sheet_by_name(sheet)
-        ws['I1'] = 'Сумма'
-        ws['I1'].font = Font(bold=True)
-        ws['I2'] = summ
-        wb.save(file_name)
-        wb.close()
+def create_year_excel_file(date: datetime.date):
+    filename = f'{date.year}.xlsx'
+    data = Task.get_by_year(date)
+    if not data:
+        return
+    create_excel_file(data, filename)
+    return filename
+
+
+def create_all_excel_file():
+    filename = 'all.xlsx'
+    data = Task.get_all_tasks()
+    if not data:
+        return
+    create_excel_file(data, filename)
+    return filename
